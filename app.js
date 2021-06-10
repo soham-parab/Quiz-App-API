7406f2a70232b99d4f3d07ad80ec4b10a0498016
soham-parab <parabsoham@gmail.com>
2021-06-10 22:34:14 +0530

    added connection to mongoDB database

--- app.js
+++ app.js
@@ -3,16 +3,29 @@ const cors = require("cors");
 const app = express();
 const quizRouteOne = require("./routes/QuizOne.route");
 const quizRouteTwo = require("./routes/QuizTwo.route");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 3500;
 
+require("dotenv/config");
+
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
    res.send("Hello Express app!");
 });
 app.use("/quizone", quizRouteOne);
 app.use("/quiztwo", quizRouteTwo);
+
+mongoose.connect(
+   process.env.DB_CONNECTION,
+   { useNewUrlParser: true, useUnifiedTopology: true },
+   () => {
+      console.log("connected to db!");
+   }
+);
 
 app.listen(PORT),
    () => {
